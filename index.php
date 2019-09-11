be98ce4b60cd5d4260750cf32ce810912e709702
luke munnich <lukemunnich@gmail.com>
2019-09-11 13:22:45 +0200

    created hotel selections

--- index.php
+++ index.php
@@ -16,7 +16,13 @@ session_start();
     <title>hotel booking</title>
 </head>
 <body>
-
+<h1>choose your dream Holiday</h1>
+<div class="price">
+<p id="inn">Holiday Inn: R 200 pp per night</p>
+<p id="rad">Ranger Bay Hotel: R 100 pp per night</p>
+<p id="city">City Family lodge: R 400 per night 4(beds)</p>
+<p id="town">City Main Town Lodge: R 150 pp per night</p>
+</div>
 
 
 </body>
