179342141eccd63a3115f521499eaa9430b35a94
luke munnich <lukemunnich@gmail.com>
2019-09-11 14:30:50 +0200

    added orign.php

--- connect.php
+++ connect.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html>
-<body>
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 
 <?php
 
-require_once 'base.php';
+require_once 'origin.php';
 
 //create connection. First need to instantiate new mysqli class
 $conn =  new mysqli("localhost", "root", "", "hotels");
-
 
 if ($conn->connect_error){
     die("Connection failed:". $conn->connect_error);
---
+++ origin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+<?php
+
+  define ("DB_SERVER","localhost");
+  define ("DB_USER","root");
+  define ("DB_PASSWORD","");
+  define ("DB_NAME","hotels");
