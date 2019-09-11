99256f2e47288fec63b2ed5147bcc76502252a9f
luke munnich <lukemunnich@gmail.com>
2019-09-11 13:16:02 +0200

    linked css files and added head

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
+
+<?php
+// Start the session
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <title>hotel booking</title>
-
-
-    
 </head>
 <body>
-    
+
+
+
 </body>
 </html>
